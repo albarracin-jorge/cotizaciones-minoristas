aaf9ac4accbcce772e708b96a5c7c8a9dcb61ea3
Jorge Albarracin <albarracinj.dev@gmail.com>
2025-05-26 21:58:52 -0300

    [add] add info support for mobile, clear logs in browser console

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -3,51 +3,55 @@ import { bankList } from "@/utils/bankList";
 import { InfoPopup } from "./InfoPopup";
 
 type CardProps = {
-    quote: Quote;
-    previousQuote: Quote;
+  quote: Quote;
+  previousQuote: Quote;
 };
 export const Card = ({ quote, previousQuote }: CardProps) => {
-    console.log(quote, previousQuote);
-    return(
-        <div className={`
+  return (
+    <div className={`
             flex flex-col items-center justify-between rounded-lg shadow-lg w-72 h-32 
             ${quote.bankName && bankList[quote.bankName as keyof typeof bankList]?.cardStyle ? bankList[quote.bankName as keyof typeof bankList].cardStyle : "bg-gray-200"}`
-        }>
-            <div className={`m-4 mt-6 text-2xl font-bold flex`}>
-                <h2 className="pr-2">
-                {quote.bankName && bankList[quote.bankName as keyof typeof bankList]?.name ? bankList[quote.bankName as keyof typeof bankList].name : "Unknown Bank"}
-                </h2>
-                <InfoPopup quote={quote} previousQuote={previousQuote} />
-            </div>
-            <div className={`w-full py-1.5 rounded-b-lg bg-[rgba(0,0,0,0.3)]`}>
-                <div className="flex items-center justify-center gap-4">
-                    <p>
-                        Compra: ${quote.buy || "N/A"}
-                        {quote.buy !== previousQuote.buy ? (
-                            <span className={`text-sm ${quote.buy! > previousQuote.buy! ? "text-green-500" : "text-red-500"}`}>
-                                {quote.buy! > previousQuote.buy! ? " ▲" : " ▼"}
-                            </span>
-                        ) : (
-                            <span className="text-xl font-bold text-gray-500">
-                                {` - `}
-                            </span>
-                        )}
-                    </p>
-                    <p>
-                        Venta: ${quote.sell || "N/A"}
-                        {quote.sell !== previousQuote.sell ? (
-                            <span className={`text-sm ${quote.sell! > previousQuote.sell! ? "text-green-500" : "text-red-500"}`}>
-                                {quote.sell! > previousQuote.sell! ? " ▲" : " ▼"}
-                            </span>
-                        ) : (
-                            <span className="text-xl font-bold text-gray-500">
-                                {` - `}
-                            </span>
-                        )}
-                    </p>
-                </div>
-            </div>
+    }>
+      <div className={`m-4 mt-6 text-2xl font-bold flex`}>
+        <h2 className="pr-2">
+          {quote.bankName && bankList[quote.bankName as keyof typeof bankList]?.name ? bankList[quote.bankName as keyof typeof bankList].name : "Unknown Bank"}
+        </h2>
+        <InfoPopup quote={quote} previousQuote={previousQuote} />
+      </div>
+      <div className={`w-full py-1.5 rounded-b-lg bg-[rgba(0,0,0,0.3)]`}>
+        <div className="lg:hidden flex items-center justify-center ml-2">
+          <span className="text-sm text-gray-500">
+            Ult. actualización: {new Date(quote.date!).toLocaleDateString() + " " + quote.hour?.split(":").slice(0, 2).join(":")}
+          </span>
         </div>
-    )
+        <div className="flex items-center justify-center gap-4">
+          <p>
+            Compra: ${quote.buy || "N/A"}
+            {quote.buy !== previousQuote.buy ? (
+              <span className={`text-sm ${quote.buy! > previousQuote.buy! ? "text-green-500" : "text-red-500"}`}>
+                {quote.buy! > previousQuote.buy! ? " ▲" : " ▼"}
+              </span>
+            ) : (
+              <span className="text-xl font-bold text-gray-500">
+                {` - `}
+              </span>
+            )}
+          </p>
+          <p>
+            Venta: ${quote.sell || "N/A"}
+            {quote.sell !== previousQuote.sell ? (
+              <span className={`text-sm ${quote.sell! > previousQuote.sell! ? "text-green-500" : "text-red-500"}`}>
+                {quote.sell! > previousQuote.sell! ? " ▲" : " ▼"}
+              </span>
+            ) : (
+              <span className="text-xl font-bold text-gray-500">
+                {` - `}
+              </span>
+            )}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
--- app/components/InfoPopup.tsx
+++ app/components/InfoPopup.tsx
@@ -7,30 +7,30 @@ import {
 import { formatDate, formatTime } from "@/lib/utils";
 import { Quote } from "../../db/schema";
 
-export const InfoPopup = ({ quote, previousQuote }: {quote: Quote; previousQuote: Quote}) => {
-    return (
-        <TooltipProvider>
-            <Tooltip>
-                <TooltipTrigger className="">
-                    <InfoIcon />
-                </TooltipTrigger>
-                <TooltipContent className="w-48">
-                    <div className="flex flex-col items-center">
-                        <p className="text-sm font-bold">Ultima actualización</p>
-                        <p className="text-xs">{`${formatDate(quote.date!)} ${formatTime(quote.hour!)}`}</p>
-                        <i>Var. compra: {(((quote.buy! - previousQuote.buy!) / previousQuote.buy!) * 100).toFixed(2)}%</i>
-                        <i>Var. venta: {(((quote.sell! - previousQuote.sell!) / previousQuote.sell!) * 100).toFixed(2)}%</i>
-                    </div>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
-    )
+export const InfoPopup = ({ quote, previousQuote }: { quote: Quote; previousQuote: Quote }) => {
+  return (
+    <TooltipProvider>
+      <Tooltip >
+        <TooltipTrigger className="hidden lg:block">
+          <InfoIcon />
+        </TooltipTrigger>
+        <TooltipContent className="w-48">
+          <div className="flex flex-col items-center">
+            <p className="text-sm font-bold">Ultima actualización</p>
+            <p className="text-xs">{`${formatDate(quote.date!)} ${formatTime(quote.hour!)}`}</p>
+            <i>Var. compra: {(((quote.buy! - previousQuote.buy!) / previousQuote.buy!) * 100).toFixed(2)}%</i>
+            <i>Var. venta: {(((quote.sell! - previousQuote.sell!) / previousQuote.sell!) * 100).toFixed(2)}%</i>
+          </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
 }
 
 const InfoIcon = () => {
-    return (
-        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
-        </svg>
-    )
+  return (
+    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+    </svg>
+  )
 }
